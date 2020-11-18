63918c42a5cb1e2d7e9cc53bb53b53ea4d148e11
NenadPantelic <nenadpantelickg@gmail.com>
2020-11-18 18:22:22 +0100

    #6 | Add auth middleware

---
+++ intern_xplorer_api/intern_xplorer/middlewares.py
@@ -0,0 +1,45 @@
+from .auth import *
+from rest_framework import status
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from .utils import get_url
+
+# TODO add job posts to auth urls
+non_auth_urls = ["login", "logout"]
+auth_urls = ["resources"]
+
+
+class AuthMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_view(self, request, view_func, *view_args, **view_kargs):
+        current_url = get_url(request).split('/')[0]
+        if current_url in auth_urls:
+            # dummy check at this point
+            # TODO: improve auth check
+            if request.method in ("POST", "PUT", "PATCH", "DELETE") and not is_admin(request):
+                return self._generate_401_unauthenticated()
+
+    def _generate_401_unauthenticated(self, message="You're not authenticated to access this resource."):
+        return self._generate_response(message, status.HTTP_401_UNAUTHORIZED)
+
+    def _generate_403_unauthorized(self, message="You're not authorized to perform this action."):
+        return self._generate_response(message, status.HTTP_403_FORBIDDEN)
+
+    def _generate_response(self, message, status):
+        response = Response(
+            {"message": message},
+            content_type="application/json",
+            status=status,
+        )
+        response.accepted_renderer = JSONRenderer()
+        response.accepted_media_type = "application/json"
+        response.renderer_context = {}
+        return response
