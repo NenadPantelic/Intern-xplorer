496ccc4b28f17aeb0dcc566d1cdd1f21b687a882
Temitope Akinsoto <takinsoto@gmail.com>
2020-11-18 23:26:04 +0100

    Create barebone Navigation component

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navigation from './Navigation'
 
 export default function Dashboard() {
     return (
         <div>
+            <Navigation />
             <h1>This is our Dashboard</h1>
         </div>
     )
