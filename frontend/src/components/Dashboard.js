5915903ca5036240c250b583439369e59512707c
Temitope Akinsoto <takinsoto@gmail.com>
2020-11-19 19:54:45 +0100

    display list of job post on the dashboard using custom job component

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,6 +4,87 @@ import { Container, Button, Header } from "semantic-ui-react";
 import Navigation from "./Navigation";
 import recruitment_image from "../assets/recruitment_bg_image.jpg";
 
+const apiResult = [
+  {
+    company: "Major League Hacking",
+    title: "Frontend Developer",
+    jobType: "Full time",
+    src: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/girl_avatar_child_kid-512.png",
+  },
+  {
+    company: "Major League Hacking",
+    title: "Software Developer",
+    jobType: "Full time",
+    src: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/girl_avatar_child_kid-512.png",
+  },
+  {
+    company: "Major League Hacking",
+    title: "Software Engineer",
+    jobType: "Full time",
+    src: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/girl_avatar_child_kid-512.png",
+  },
+  {
+    company: "Major League Hacking",
+    title: "DevOps Engineer",
+    jobType: "Full time",
+    src: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/girl_avatar_child_kid-512.png",
+  },
+  {
+    company: "Major League Hacking",
+    title: "Software Engineer",
+    jobType: "Full time",
+    src: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/girl_avatar_child_kid-512.png",
+  },
+  {
+    company: "Major League Hacking",
+    title: "DevOps Engineer",
+    jobType: "Full time",
+    src: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/girl_avatar_child_kid-512.png",
+  },
+  {
+    company: "Major League Hacking",
+    title: "Software Engineer",
+    jobType: "Full time",
+    src: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/girl_avatar_child_kid-512.png",
+  },
+  {
+    company: "Major League Hacking",
+    title: "DevOps Engineer",
+    jobType: "Full time",
+    src: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/girl_avatar_child_kid-512.png",
+  },
+  {
+    company: "Major League Hacking",
+    title: "Software Engineer",
+    jobType: "Full time",
+    src: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/girl_avatar_child_kid-512.png",
+  },
+  {
+    company: "Major League Hacking",
+    title: "DevOps Engineer",
+    jobType: "Full time",
+    src: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/girl_avatar_child_kid-512.png",
+  }
+];
+
+function Job(prop) {
+  const { company, title, jobType, src } = prop.details;
+  return (
+    <div style={{display: "flex", boxShadow: "1px 3px 5px 1px gray", width: "60%", margin: "1rem auto", padding: "1rem"}}>
+      <div style={{width: "10%"}}>
+        <img src={src} width="100%" />
+      </div>
+      <div style={{width: "90%", textAlign: "left", paddingLeft: "2rem"}}>
+        <p style={{fontSize: "16px"}}>{company}</p>
+        <div>
+          <h3>{title}</h3>
+          <p style={{fontSize: "16px"}}>{jobType}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Dashboard() {
   return (
     <div>
@@ -47,6 +128,9 @@ export default function Dashboard() {
           </Link>
         </container>
       </div>
+      {apiResult.map((job) => (
+        <Job details={job} />
+      ))}
     </div>
   );
 }
