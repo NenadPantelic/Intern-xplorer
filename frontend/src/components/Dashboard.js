81013cddaa9659a663faef5090e614423106d498
Temitope Akinsoto <takinsoto@gmail.com>
2020-11-19 01:07:59 +0100

    Add more dashboard styles

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,11 +1,51 @@
-import React from 'react'
-import Navigation from './Navigation'
+import React from "react";
+import {
+  Container,
+  Button,
+  Header,
+} from "semantic-ui-react";
+import Navigation from "./Navigation";
+import recruitment_image from "../assets/recruitment_bg_image.jpg";
 
 export default function Dashboard() {
-    return (
-        <div>
-            <Navigation />
-            <h1>This is our Dashboard</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Navigation />
+      <div style={{ height: "90vh" }}>
+        <img width="100%" height="100%" src={recruitment_image} />
+        <container
+          text
+          style={{
+            position: "absolute",
+            width: "70%",
+            top: "30%",
+            left: "20%",
+          }}
+        >
+          <Header
+            content="Intern Explorer"
+            inverted
+            style={{
+              fontWeight: "normal",
+              marginBottom: 0,
+              fontSize: "4em",
+            }}
+          />
+          <Header
+            content="Do whatever you want when you want"
+            inverted
+            style={{
+              fontWeight: "normal",
+              fontSize: "1.5em",
+              marginBottom: "2em",
+            }}
+          />
+          <Button primary size="big">
+            Sign Up
+          </Button>
+          <Button size="big">Login</Button>
+        </container>
+      </div>
+    </div>
+  );
 }
